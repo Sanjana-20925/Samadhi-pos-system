9e60ed829fbd85d9f8822dd7dccd128e9358d002
Tharu-fernando <tharukafernando27@gmail.com>
2026-09-07 17:18:00 +0530

    First Code

---
+++ src/main/java/com/mycompany/samadhi_pos/Samadhi_POS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.samadhi_pos;
+
+/**
+ *
+ * @author tharu
+ */
+public class Samadhi_POS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
